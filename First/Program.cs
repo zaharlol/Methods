c02adfb15f2114509f2b285d3ebb5f302467d52d
zaharlol <kovtun.zahar12@mail.ru>
2023-03-17 20:13:07 +0300

    Проверочное задание

--- First/Program.cs
+++ First/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Net.Security;
+using System.Reflection;
+using System.Xml.Linq;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        (string name, string[] dishes) User;
+
+        Console.WriteLine("Введите ваше имя");
+        User.name = Console.ReadLine();
+
+        Console.WriteLine("Введите 5 любимых блюд");
+        User.dishes = new string[5];
+        for (int i = 0; i < 5; i++)
+        {
+            User.dishes[i] = Console.ReadLine();
+        }
+    }
+}
