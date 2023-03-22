3e484a55b5a0d10ace7509d752123d3dcef0b804
zaharlol <kovtun.zahar12@mail.ru>
2023-03-22 12:55:08 +0300

    Возведение в степень

--- Рекурсия/Program.cs
+++ Рекурсия/Program.cs
@@ -9,12 +9,51 @@ class MainClass
         {
             modif = modif.Remove(0, 2);
         }
+
+        Console.BackgroundColor = (ConsoleColor)deep;
         Console.WriteLine("..." + modif);
+
         if (deep > 1)
         {
             Echooo(modif, deep - 1);
         }
     }
+
+    static decimal Factorial(int x)
+    {
+        if (x == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return x * Factorial(x - 1);
+        }
+    }
+
+    private static int PowerUp(int N, byte pow)
+    {
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                if (pow == 1)
+                {
+                    return N;
+                }
+                else
+                {
+                    return N * PowerUp(N, --pow);
+                }
+            }
+        }
+    }
+
+
+
     static void Main(string[] args)
     {
         Console.WriteLine("Напишите что-то");
@@ -26,5 +65,11 @@ class MainClass
         Echooo(str, deep);
 
         Console.ReadKey();
+
+        Console.WriteLine("Введие число");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите степень");
+        byte b = Convert.ToByte(Console.ReadLine());
+        PowerUp(a, b);
     }
 }
